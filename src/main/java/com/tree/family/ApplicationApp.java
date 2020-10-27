f8a92326283268c4ef7cc614f3d0e8e94646d25a
hodong.kim <fship1124@gmail.com>
2020-10-27 18:22:48 +0900

    boot init

---
+++ src/main/java/com/tree/family/ApplicationApp.java
@@ -0,0 +1,12 @@
+package com.tree.family;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationApp.class, args);
+        System.out.println("family tree started!!");
+    }
+}
